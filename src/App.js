560d7343b680b8a2a18d4c19dfc7efa5ac6dc365
Emmanuel Danso Annan <emmanueldansoannan@gmail.com>
2023-01-04 02:21:38 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const url = 'https://api.github.com/users/Edannan';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const [loading, setLoading] = useState(true);
+const [error, setError] = useState(false);
+const [user, setUser] = useState({login:'default user', name:'', avatar_url:''});
+
+useEffect(()=>{
+  fetch(url)
+  .then((data)=>{
+    if (data.status >= 200 && data.status <= 299) {
+    return data.json();
+    }else{
+      setLoading(false);
+      setError(true);
+    }
+  })
+  .then((gituser)=>{
+    const{login, name, avatar_url} = gituser;
+    setUser({...user, login, name, avatar_url});
+    setLoading(false);
+
+  })
+  .catch((error)=>{
+    console.log(error);
+  })
+},[])
+
+if(loading)
+return (
+  <div className="profile container">
+    <div className="profile">
+      <h1>Loading...</h1>
+    </div>
+  </div>
+)
+else if(error)
+    return (
+    <div className="profile container">
+      <div className="profile">
+        <h1>Error...</h1>
+      </div>
     </div>
   );
+
+  return (
+    <div className="profile container">
+      <div className="profile">
+        <img src={user.avatar_url} alt="avi"/>
+        <h1>{user.login}</h1>
+        <h1>{user.name}</h1>
+      </div>
+    </div>
+  )
 }
 
 export default App;
